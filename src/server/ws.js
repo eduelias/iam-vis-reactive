84268d65a80dabe145ee5d1a3373cf87b34c2259
Eduardo Elias Saleh <eduardo@lab15.io>
2018-03-26 11:08:36 +0200

    websockets

--- src/server/ws.js
+++ src/server/ws.js
@@ -1,30 +1,74 @@
 var express = require('express')
 var ws = require('./ws')
+var vis = require('vis');
 var app = express()
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/ws.html');
+    res.sendfile(__dirname + '/views/ws.html');
 })
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!')
 })
+var dataobj = [{
+        "x": -117,
+        "y": -24,
+        "id": "0",
+        "title": "informations",
+        "connections": [
+            1,
+            4
+        ]
+    },
+    {
+        "x": -7,
+        "y": 3,
+        "id": "1",
+        "connections": [
+            0,
+            2,
+            4
+        ]
+    },
+    {
+        "x": 97,
+        "y": 40,
+        "id": "2",
+        "connections": [
+            1,
+            3
+        ]
+    },
+    {
+        "x": 201,
+        "y": 88,
+        "id": "3",
+        "connections": [
+            2
+        ]
+    },
+    {
+        "x": -42,
+        "y": -109,
+        "id": "4",
+        "connections": [
+            0,
+            1
+        ]
+    }
+];
 
 var WebSocketServer = require('ws').Server,
     wss = new WebSocketServer({ port: 40510 })
 wss.on('connection', function(ws) {
     ws.on('message', function(message) {
-        ws.send(JSON.stringify({
-            nodes: [{ id: 1, label: 'Node 1' },
-                { id: 2, label: 'Node 2' },
-                { id: 3, label: 'Node 3' },
-                { id: 4, label: 'Node 4' },
-                { id: 5, label: 'Node 5' }
-            ],
-            edges: [{ from: 1, to: 3 },
-                { from: 1, to: 2 },
-                { from: 2, to: 4 },
-                { from: 2, to: 5 },
-                { from: 3, to: 3 }
-            ]
-        }));
+        var data = {}
+        try {
+            data = JSON.parse(message);
+            dataobj = data;
+        } catch (e) {
+            console.log(e);
+        }
+        wss.clients.forEach(function each(ws) {
+            ws.send(JSON.stringify(dataobj));
+        })
     })
 })
